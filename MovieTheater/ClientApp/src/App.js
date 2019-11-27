b7040ef04da949738aec031cf7fd11cda9771488
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2019-11-27 21:49:06 +0200

    Add login and register pages

--- MovieTheater/ClientApp/src/App.js
+++ MovieTheater/ClientApp/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 import Home from './components/Home';
+import Login from './components/Authentication/Login';
+import Register from './components/Authentication/Register';
+import { PrivateRoute } from './components/Authentication/PrivateRoute';
 
 export default () => (
-  <Route exact path='/' component={Home} />
+  <BrowserRouter>
+    <Switch>
+      <PrivateRoute exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Switch>
+  </BrowserRouter>
 );
